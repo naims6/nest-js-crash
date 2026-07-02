41bfb97fd19eefa835b2192a1bfe05ec0429ad78
naims <naimdrive6@gmail.com>
2026-07-02 12:48:05 +0600

    Update dependencies and fix module imports

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // app.enableCors();
+  // app.use(...);
+  // app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+await bootstrap();
