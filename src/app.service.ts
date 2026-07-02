0fa0ac5d87ba0130ff1b062f02ed46ec98109f78
naims <naimdrive6@gmail.com>
2026-07-02 19:56:00 +0600

    Add user retrieval methods to AppService

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,23 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getAllUsers() {
+    return [
+      {
+        id: 133,
+        name: 'Naim Sorker',
+        age: 21,
+      },
+      { id: 134, name: 'Naims 2', age: 25 },
+      { id: 135, name: 'Naimfadf 2', age: 24 },
+      { id: 136, name: 'Naimdfad 2', age: 25 },
+    ];
+  }
+
+  getUserProfile(id: string) {
+    return {
+      id: id,
+    };
+  }
 }
